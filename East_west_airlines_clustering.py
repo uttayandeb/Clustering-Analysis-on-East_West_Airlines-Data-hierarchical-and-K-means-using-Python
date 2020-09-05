bda6ba1ee8b50c84f86394d3f5f62719c8e44fe0
Uttayan Deb <52741280+uttayandeb@users.noreply.github.com>
2020-09-05 21:45:35 +0530

    Add files via upload

---
+++ East_west_airlines_clustering.py
@@ -0,0 +1,153 @@
+
+##################     Hierarchical Clustering ################
+
+
+
+
+import pandas as pd
+import matplotlib.pylab as plt 
+Airlines = pd.read_csv("C:\\Users\\home\\Desktop\\Data Science Assignments\\Clustering\\New folder\\EastWestAirlines.csv")
+Airlines
+
+### Excluding the first column and only taking the numerical value of the data set
+Sub_Airlines=Airlines.iloc[:,1:]
+Sub_Airlines
+# Normalization function (when we have both catagorical and non-catagorical datas)
+#or normalize the data in the range of 0 and 1
+def norm_func1(i):
+    x = (i-i.min())	/	(i.max()	-	i.min())
+    return (x)
+
+# alternative normalization function (Z-transformation)
+#normalizing in the range of -2.5 to +2.5
+
+def norm_func(i):
+    x = (i-i.mean())/(i.std())
+    return (x)
+
+# Normalized data frame (considering the numerical part of data)
+df_norm = norm_func(Airlines.iloc[:,1:])
+df_norm
+
+
+from scipy.cluster.hierarchy import linkage 
+
+import scipy.cluster.hierarchy as sch # for creating dendrogram 
+
+type(df_norm)#to know the datatype
+
+import numpy as np
+p = np.array(df_norm) # converting into numpy array format 
+p
+
+
+
+
+help(linkage)#to know about linkage
+#linkage. Performs hierarchical/agglomerative clustering on the condensed distance matrix y. sized vector where n is the number of original observations paired in the distance matrix
+z = linkage(df_norm, method="complete",metric="euclidean")
+z
+
+
+
+plt.figure(figsize=(15, 10));plt.title('Hierarchical Clustering Dendrogram');plt.xlabel('Index');plt.ylabel('Distance')
+sch.dendrogram(
+    z,
+    leaf_rotation=0.,  # rotates the x axis labels
+    leaf_font_size=8.,  # font size for the x axis labels
+)
+plt.show()
+
+
+# Now applying AgglomerativeClustering choosing 3 as clusters from the dendrogram
+from	sklearn.cluster	import	AgglomerativeClustering 
+h_complete	=	AgglomerativeClustering(n_clusters=3,	linkage='complete',affinity = "euclidean").fit(df_norm) 
+h_complete
+
+cluster_labels=pd.Series(h_complete.labels_)
+
+Airlines['clust']=cluster_labels # creating a  new column and assigning it to new column 
+Airlines
+Airlines.iloc[:,[12,0,1,2,3,4,5,6,7,8,9,10,11]]
+Airlines
+Airlines.head()
+
+# getting aggregate mean of each cluster
+Airlines.iloc[:,2:].groupby(Airlines.clust).median()
+
+# creating a csv file 
+Airlines.to_csv("EastWestAirlines.csv",encoding="utf-8")
+
+
+
+
+
+
+
+
+
+
+
+############        K-means clustering              ###################
+
+
+
+from	sklearn.cluster	import	KMeans
+from scipy.spatial.distance import cdist 
+import numpy as np
+
+# Generating random uniform numbers 
+X = np.random.uniform(0,1,1000)
+X
+Y = np.random.uniform(0,1,1000)
+df_xy =pd.DataFrame(columns=["X","Y"])
+
+df_xy.X = X
+
+df_xy.Y = Y
+df_xy.plot(x="X",y = "Y",kind="scatter")
+
+###########generating kmeans model with 5 clusters
+
+model1 = KMeans(n_clusters=5).fit(df_xy)
+model1.labels_
+model1.cluster_centers_
+df_xy.plot(x="X",y = "Y",c=model1.labels_,kind="scatter",s=10,cmap=plt.cm.coolwarm)
+
+
+
+
+df_norm.head(10)  # Top 10 rows
+
+###### screw plot or elbow curve ############
+k = list(range(2,15))
+k
+TWSS = [] # variable for storing total within sum of squares for each kmeans 
+for i in k:
+    kmeans = KMeans(n_clusters = i)
+    kmeans.fit(df_norm)
+    WSS = [] # variable for storing within sum of squares for each cluster 
+    for j in range(i):
+        WSS.append(sum(cdist(df_norm.iloc[kmeans.labels_==j,:],kmeans.cluster_centers_[j].reshape(1,df_norm.shape[1]),"euclidean")))
+    TWSS.append(sum(WSS))
+
+
+# Scree plot 
+plt.plot(k,TWSS, 'ro-');plt.xlabel("No_of_Clusters");plt.ylabel("total_within_SS");plt.xticks(k)
+
+# Selecting 5 clusters from the above scree plot which is the optimum number of clusters 
+model=KMeans(n_clusters=5) 
+model.fit(df_norm)
+
+
+model.labels_ # getting the labels of clusters assigned to each row 
+md=pd.Series(model.labels_)  # converting numpy array into pandas series object 
+Airlines['clust']=md # creating a  new column and assigning it to new column 
+df_norm.head()
+
+Airlines= Airlines.iloc[:,[12,0,1,2,3,4,5,6,7,8,9,10,11]]
+Airlines
+Airlines.iloc[:,1:11].groupby(Airlines.clust).mean()
+
+Airlines.to_csv("EastWestAirlines.csv")
+
